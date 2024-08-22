1301c253096a088e3d20e5ada0a62823769b9136
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2024-08-22 13:54:42 +0200

    Added base action

--- .github/actions/base/entrypoint.sh
+++ .github/actions/base/entrypoint.sh
@@ -20,7 +20,7 @@ fi
 python3 -m venv .venv
 source .venv/bin/activate
 
-ll -la
+ls -la
 
 # Install Python dependencies
 find ros2_ws/src/* -maxdepth 3 -name requirements.txt -exec pip install -r {} \;
