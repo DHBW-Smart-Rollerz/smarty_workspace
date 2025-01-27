29dd16adb43c25629c33d776dde5f61fa56d5ba7
Jonas Weihing <jonas.weihing@outlook.de>
2025-01-27 20:19:59 +0100

    Fixes bug where ros2 is not using the correct pyenv

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,12 @@ BUILD_TYPE=RelWithDebInfo
 
 # Check if the user has provided a package name
 if [ "$#" -gt 0 ]; then
-    colcon build \
+    python -m colcon build \
     --symlink-install \
     --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
     -Wall -Wextra -Wpedantic --packages-select "$@"
 else
-    colcon build \
+    python -m colcon build \
     --symlink-install \
     --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
     -Wall -Wextra -Wpedantic
